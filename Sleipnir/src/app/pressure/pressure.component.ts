aff138b10a084754315d3b29964ef36efbdbc1ae
Martin Stenflo <stenflo@gmail.com>
2024-06-02 14:31:28 -0600

    add valve control

--- Sleipnir/src/app/pressure/pressure.component.ts
+++ Sleipnir/src/app/pressure/pressure.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { WebsocketService } from '../services/websocket.service';
+import { Inject } from '@angular/core';
+
 @Component({
   selector: 'app-pressure',
   standalone: true,
@@ -8,5 +11,15 @@ import { Component } from '@angular/core';
   styleUrl: './pressure.component.css'
 })
 export class PressureComponent {
+  constructor(@Inject(WebsocketService) private websocketService: WebsocketService) {} // Add the @Inject decorator
 
+  public valveControl(unit: string, location: string, action: string): void {
+    const payload = {
+      location: location,
+      action: action,
+      unit: unit
+    };
+    console.log('Sending payload:', JSON.stringify(payload));
+    this.websocketService.sendMessage(JSON.stringify(payload));
+  }
 }
---
+++ Sleipnir/src/app/services/websocket.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebsocketService {
+  private socket: WebSocket;
+
+  constructor() {
+    this.socket = new WebSocket('ws://localhost:8080');
+
+    this.socket.onopen = () => {
+      console.log('WebSocket connection established');
+    };
+
+    this.socket.onmessage = (event) => {
+      console.log('Message received:', event.data);
+    };
+
+    this.socket.onclose = () => {
+      console.log('WebSocket connection closed');
+    };
+
+    this.socket.onerror = (error) => {
+      console.error('WebSocket error:', error);
+    };
+  }
+  
+    public sendMessage(message: string): void {
+      const messageToSend = JSON.stringify({ message });
+      this.socket.send(messageToSend);
+   }
+}
