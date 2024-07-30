9b1701e415cab5ff7662a67d0fd5027327a847fc
Martin Stenflo <martinstenflo@Martins-Mac-mini.local>
2024-07-29 21:44:35 -0600

    add patterns screen

--- Sleipnir/src/app/monitor/monitor.component.ts
+++ Sleipnir/src/app/monitor/monitor.component.ts
@@ -11,8 +11,6 @@ import { NgClass } from '@angular/common';
 })
 export class MonitorComponent {
 
-  public isPressed: any = {};
-
   constructor(@Inject(WebsocketService) private websocketService: WebsocketService) {}
 
   public buttonPress(event: Event, action: string, side: string): void {
@@ -20,17 +18,14 @@ export class MonitorComponent {
     const element = (event.target as HTMLElement).closest('.button');
 
     if (action === 'hold') {
-      this.isPressed[action + side] = false;
       if (element) {
         element.classList.remove('pressed');
       }
     } else {
-      this.isPressed[action + side] = true;
       if (element) {
         element.classList.add('pressed');
       }
     }
-    this.isPressed[action + side] = false;
     const payload = {
       "type": "pneumaticsCommandText",
       "command": action + side,
@@ -38,38 +33,5 @@ export class MonitorComponent {
     }
     console.log(JSON.stringify(payload));
     this.websocketService.sendMessage(JSON.stringify(payload));
-  }
-
-  public move(side: string): void {
-    this.isPressed[side] = true;
-    const payload = {
-      "type": "pneumaticsCommandText",
-      "command": side,
-      "sendTime": Date().toLocaleString()
-    }
-    console.log(JSON.stringify(payload));
-    this.websocketService.sendMessage(JSON.stringify(payload))
-  }
-
-  public hold(side: string): void {
-    this.isPressed['raise' + side] = false;
-    this.isPressed['lower' + side] = false;
-    const payload = {
-      "type": "pneumaticsCommandText",
-      "command": 'hold' + side,
-      "sendTime": Date().toLocaleString()
-    }
-    console.log(JSON.stringify(payload));
-    this.websocketService.sendMessage(JSON.stringify(payload))
-  }
-
-  touchStart(event: TouchEvent, position: string) {
-    event.preventDefault();
-    this.isPressed[position] = true;
-  }
-
-  touchEnd(event: TouchEvent, position: string) {
-    event.preventDefault();
-    this.isPressed[position] = false;
   }
 }
