a01ba79d98d65fe61230a39076154c4e6f69c57d
alexmakijokela <alex.makijokela@gmail.com>
2024-08-04 16:32:04 -0600

    patch: -0.0.0.0

--- Sleipnir/src/app/services/websocket.service.ts
+++ Sleipnir/src/app/services/websocket.service.ts
@@ -7,7 +7,7 @@ export class WebsocketService {
   private socket: WebSocket;
 
   constructor() {
-    this.socket = new WebSocket('ws://127.0.0.1:8078');
+    this.socket = new WebSocket('ws://0.0.0.0:8078');
 
     this.socket.onopen = () => {
       console.log('WebSocket connection established');
