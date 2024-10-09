921e70ce23e7853c8a9e043d9914e7113b9ce85e
Kyle Stewart <kgstew@gmail.com>
2024-10-09 11:14:07 -0700

    Add new route and link in header for waves

--- Sleipnir/src/app/app.component.ts
+++ Sleipnir/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
+import { HeaderComponent } from './header/header.component';
 import { LightComponent } from './light/light.component';
 import { PressureComponent } from './pressure/pressure.component';
 import { MonitorComponent } from './monitor/monitor.component';
@@ -8,13 +8,25 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { CommonModule } from '@angular/common';
 import { PatternsComponent } from './patterns/patterns.component';
 import { FormsModule } from '@angular/forms';
+import { WavesComponent } from './waves/waves.component';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [RouterOutlet, CommonModule, FormsModule, HeaderComponent, LightComponent, PressureComponent, MonitorComponent, DashboardComponent, PatternsComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [
+    RouterOutlet,
+    WavesComponent,
+    CommonModule,
+    FormsModule,
+    HeaderComponent,
+    LightComponent,
+    PressureComponent,
+    MonitorComponent,
+    DashboardComponent,
+    PatternsComponent,
+  ],
 })
 export class AppComponent {
   title = 'Sleipnir';
