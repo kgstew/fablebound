6ee7b54c8d898901fa8d5b5ab432f2f1bff6bd52
Matt Levitan <matt.levitan@mammothgrowth.com>
2025-05-02 21:01:59 -0600

    updates sleipnir and Yggdrasil code to support distnce sensors

---
+++ Yggdrasil/src/domain/models/distance-reading.ts
@@ -0,0 +1,9 @@
+import { AssemblyName } from './assembly-name'
+
+type DistanceReading = {
+    assemblyName: AssemblyName
+    readingDistance: number
+    readDate: Date
+}
+
+export { DistanceReading }
